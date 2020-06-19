13ed8fb68ad519e4e5c45b577cb8f8779a90e42b
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2020-06-19 17:23:59 -0400

    uploading favicon sizes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import header from '../img/header.png';
 
 class NavBar extends React.Component {
+
   render(){
     return(
-      <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-               <div className="container">
-                   <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                      <img className="header-photo" src={ header } alt=""/>
-                   </a><button className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i className="fa fa-bars"></i></button>
-                   <div className="collapse navbar-collapse" id="navbarResponsive">
-                       <ul className="navbar-nav ml-auto">
-                           <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li>
-                           <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                           <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
-                       </ul>
-                   </div>
-               </div>
-           </nav>
+
+      <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">
+                <img className="header-photo" src={ header } alt=""/>
+            </a>
+
+                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"> Menu <i class="fas fa-bars"></i>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
     );
   }
 }
